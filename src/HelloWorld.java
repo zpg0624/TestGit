fc2b27292aaf1944779af218313b8c769973b997
zhangpg0624 <1562112890@qq.com>
2018-07-09 10:48:47 +0800

    init

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @created by zpg
+ * @since 2018-07-09 10:42:04
+ */
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+    System.out.println("aaaa");
+
+    }
+}
